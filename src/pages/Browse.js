5e765027fec5c74266e225b11714de55bfa4c035
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2020-04-10 01:00:18 +0200

    Added error handling when Github API responds with unauthenticated

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -5,6 +5,8 @@ import { InView } from "react-intersection-observer";
 import { CircularProgress } from "@material-ui/core";
 import "@github/g-emoji-element";
 import { makeStyles } from "@material-ui/core/styles";
+import firebase from "firebase";
+import "firebase/auth";
 
 import RepositoryCard from "./Browse/RepositoryCard";
 
@@ -89,6 +91,10 @@ function Browse() {
   }
 
   if (error) {
+    // Check if unauthenticated
+    if (error?.networkError?.statusCode === 401) {
+      firebase.auth().signOut();
+    }
     return <p>Error.</p>;
   }
 
